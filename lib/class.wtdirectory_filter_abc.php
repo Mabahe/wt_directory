c284845763ac6b98dcc9d6e0e0e40f6088075211
it@hk <it@hk@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-12-10 13:42:17 +0000

    wrapped "0-9" into own span (with CSS class wtdirectory_abc_numbers)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/wt_directory/branches/201204_Boris_Glusmann_Changes@68646 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.wtdirectory_filter_abc.php
+++ lib/class.wtdirectory_filter_abc.php
@@ -31,9 +31,9 @@ class tx_wtdirectory_filter_abc extends tslib_pibase {
 	var $extKey = 'wt_directory'; // Extension key
 	var $prefixId = 'tx_wtdirectory_pi1';		// Same as class name
 	var $scriptRelPath = 'pi1/class.tx_wtdirectory_pi1.php';	// Path to any script in pi1 for locallang
-	
+
 	function main($conf, $piVars, $query_pid, $query_cat) {
-		
+
 		// Config
 		global $TSFE;
     	$this->cObj = $TSFE->cObj; // cObject
@@ -45,7 +45,7 @@ class tx_wtdirectory_filter_abc extends tslib_pibase {
 		$this->dynamicMarkers = t3lib_div::makeInstance('tx_wtdirectory_dynamicmarkers'); // New object: TYPO3 dynamicmarker function
 		$this->tmpl = $this->markerArray = array(); $this->filter = ''; // init
 		$this->tmpl['filter']['abc'] = $this->cObj->getSubpart($this->cObj->fileResource($this->conf['template.']['search']), '###WTDIRECTORY_FILTER_ABC###'); // Load HTML Template
-		
+
 		// let's go
 		if ($this->pi_getFFvalue($this->conf, 'abc', 'list') != '') { // if abc should be shown
 			if (!empty($this->piVars['catfilter'])) { // if catfilter is set
@@ -59,7 +59,7 @@ class tx_wtdirectory_filter_abc extends tslib_pibase {
 					$this->filter .= ' AND tt_address_group.uid = ' . $this->piVars['catfilter']; // if catfilter set, add where clause
 				}
 			}
-			
+
 			$this->markerArray['###WTDIRECTORY_ABC_ALL###'] = $this->show_all(); // Link all
 			$this->markerArray['###WTDIRECTORY_ABC_ABC###'] = $this->show_abc(); // Link abc
 			$this->markerArray['###WTDIRECTORY_ABC_0-9###'] = $this->show_numbers(); // Link numbers
@@ -67,18 +67,18 @@ class tx_wtdirectory_filter_abc extends tslib_pibase {
 			$this->content = $this->dynamicMarkers->main($this->conf, $this->cObj, $this->content); // Fill dynamic locallang or typoscript markers
 			$this->content = preg_replace("|###.*?###|i", "", $this->content); // Finally clear not filled markers
 		}
-		
+
 		if (!empty($this->content)) return $this->content;
-		
+
     }
-	
-	
+
+
 	// Function show_abc() to generate ABC list
 	function show_abc() {
 		$content = ''; // init
-		
+
 		for ($a=A; $a != AA; $a++) { // ABC loop
-			
+
 			$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery ( // DB query
 				'tt_address.uid uid',
 				'tt_address LEFT JOIN tt_address_group_mm on (tt_address.uid = tt_address_group_mm.uid_local) LEFT JOIN tt_address_group on (tt_address_group_mm.uid_foreign = tt_address_group.uid)',
@@ -88,36 +88,35 @@ class tx_wtdirectory_filter_abc extends tslib_pibase {
 				$limit = '1'
 			);
 			$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
-			
+
 			// Generate Return string
 			$content .= '<span class="wtdirectory_abc_letter' . ($this->piVars['filter'][$this->pi_getFFvalue($this->conf, 'abc', 'list')] == strtolower($a).'%' ? ' wtdirectory_abc_letter_act' : '') . '">';
 			if($row['uid']) { // If result (link with letter)
 				$content .= $this->pi_linkTP_keepPIvars ($a, array('filter' => array($this->pi_getFFvalue($this->conf, 'abc', 'list') => htmlentities(strtolower($a) . '%') )), 1); // Generate link for each sign
 			} else { // no result: letter only link
-				$content .= $a; 
-			} 
+				$content .= $a;
+			}
 			$content .= '</span>' . "\n";
-			
+
 		}
 		if (!empty($content)) return $content;
 	}
-	
-	
+
+
 	// Function show_all() generates link same page without piVars
 	function show_all() {
 		$content = '<span class="wtdirectory_abc_letter_all' . ($this->piVars['list']['all'] || count($this->piVars) == 0 ? ' wtdirectory_abc_letter_all_act' : '') . '">';
 		$content .= $this->cObj->typolink($this->pi_getLL('wtdirectory_ll_abclist_all', 'All'), array('parameter'=>$GLOBALS['TSFE']->id, "useCacheHash" => 1, 'additionalParams' => $this->pi_getFFvalue($this->conf, 'shownone', 'mainconfig') ? '&'.$this->prefixId.'[list]=all' : ''));
-		$content .= '</span>';
-		$content .= "\n";
-		
+		$content .= '</span>' . "\n";
+
 		if (!empty($content)) return $content;
 	}
-	
-	
+
+
 	// Function show_numbers() to generate numbers link
 	function show_numbers() {
 		$content = ''; $query_numbers = ''; // init
-		
+
 		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery ( // DB query
 			'tt_address.uid uid',
 			'tt_address LEFT JOIN tt_address_group_mm on (tt_address.uid = tt_address_group_mm.uid_local) LEFT JOIN tt_address_group on (tt_address_group_mm.uid_foreign = tt_address_group.uid)',
@@ -128,19 +127,21 @@ class tx_wtdirectory_filter_abc extends tslib_pibase {
 			$limit = '1'
 		);
 		$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
-		
+
 		// Generate Return string
+		$content .= '<span class="wtdirectory_abc_numbers' . ($this->piVars['filter'][$this->pi_getFFvalue($this->conf, 'abc', 'list')] == '@' ? ' wtdirectory_abc_act' : '') . '">';
 		if($row['uid'] > 0) { // If result
 			$content .= $this->pi_linkTP_keepPIvars ($this->pi_getLL('wtdirectory_ll_abclist_numbers', '0-9'), array('filter' => array($this->pi_getFFvalue($this->conf, 'abc', 'list') => "@" )), 1); // Generate link for 0-9
 		} else { // if no result (no link)
-			$content = $this->pi_getLL('wtdirectory_ll_abclist_numbers', '0-9') . "\n"; 
-		} 
-			
+			$content .= $this->pi_getLL('wtdirectory_ll_abclist_numbers', '0-9');
+		}
+		$content .= '</span>' . "\n";
+
 		if (!empty($content)) return $content;
 	}
-	
-	
-	
+
+
+
 }
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/wt_directory/lib/class.wtdirectory_filter_abc.php'])	{
